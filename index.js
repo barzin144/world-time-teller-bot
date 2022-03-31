f70dc1717f62cb5c2fc4c9d0d04040624735c5bd
Barzin <barzin.assa@elfo.com>
2022-03-31 17:44:48 +0800

    use inline keyboard

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (token === undefined) {
 }
 const timeZones = {
   Iran: "Asia/Tehran",
-  Paris: "Europe/Paris",
+  France: "Europe/Paris",
   Malaysia: "Asia/Kuala_Lumpur",
 };
 
@@ -15,25 +15,33 @@ const bot = new Telegraf(token);
 // Set the bot response
 
 bot.command("time", (ctx) => {
+  /*
   const replyKeyboardMarkup = {
     keyboard: [Object.keys(timeZones).map((x) => ({ text: x }))],
   };
+  */
+  const inlineKeyboard = {
+    inline_keyboard: [
+      Object.keys(timeZones).map((x) => ({ text: x, callback_data: x })),
+    ],
+  };
   ctx.reply("Select a country", {
     reply_to_message_id: ctx.message.message_id,
-    reply_markup: replyKeyboardMarkup,
+    reply_markup: inlineKeyboard,
   });
 });
 
-bot.on("text", (ctx) => {
-  if (timeZones.hasOwnProperty(ctx.message.text)) {
+bot.on("callback_query", (ctx) => {
+  const selectedInlineQuery = ctx.callbackQuery.data;
+  if (timeZones.hasOwnProperty(selectedInlineQuery)) {
     ctx.reply(
-      new Date().toLocaleTimeString("en-US", {
-        timeZone: timeZones[ctx.message.text],
+      `${selectedInlineQuery}: ${new Date().toLocaleTimeString("en-US", {
+        timeZone: timeZones[selectedInlineQuery],
         hour12: false,
-      }),
+      })}`,
       {
-        reply_to_message_id: ctx.message.message_id,
-        reply_markup: { remove_keyboard: true },
+        reply_to_message_id:
+          ctx.callbackQuery.message.reply_to_message.message_id,
       }
     );
   }
@@ -43,10 +51,12 @@ const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 
 // Set telegram webhook
 bot.telegram.setWebhook(`https://world-time-teller.herokuapp.com${secretPath}`);
+//local webhook
+//bot.telegram.setWebhook(`https://tame-vampirebat-97.loca.lt${secretPath}`);
 
 const app = express();
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("This is a Telegram bot"));
 // Set the bot API endpoint
 app.use(bot.webhookCallback(secretPath));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
